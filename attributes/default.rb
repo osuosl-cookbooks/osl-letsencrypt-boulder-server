5646856404097d85212184e926da9f1fb8ac63f2
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-02 14:08:48 -0800

    Oops. Attribute precendence typo.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,10 @@
-node['boulder']['config']['boulder-config']['va']['portConfig']['httpPort'] = 80
-node['boulder']['config']['boulder-config']['va']['portConfig']['httpsPort'] = 443
-node['boulder']['config']['boulder-config']['va']['portConfig']['tlsPort'] = 443
-node['boulder']['config']['boulder-config']['syslog']['network'] = 'udp'
-node['boulder']['config']['boulder-config']['syslog']['server'] = 'localhost:514'
+default['boulder']['config']['boulder-config']['va']['portConfig']['httpPort'] = 80
+default['boulder']['config']['boulder-config']['va']['portConfig']['httpsPort'] = 443
+default['boulder']['config']['boulder-config']['va']['portConfig']['tlsPort'] = 443
+default['boulder']['config']['boulder-config']['syslog']['network'] = 'udp'
+default['boulder']['config']['boulder-config']['syslog']['server'] = 'localhost:514'
+# Default 8000 port is needed by Atlassian apps.
+default['boulder']['config']['boulder-config']['wfe']['debugAddr'] = 'localhost:7999'
 
-node['boulder']['config']['issuer-ocsp-responder']['syslog']['network'] = 'udp'
-node['boulder']['config']['issuer-ocsp-responder']['syslog']['server'] = 'localhost:514'
+default['boulder']['config']['issuer-ocsp-responder']['syslog']['network'] = 'udp'
+default['boulder']['config']['issuer-ocsp-responder']['syslog']['server'] = 'localhost:514'
