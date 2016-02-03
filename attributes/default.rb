2138f9c64d4b02828318e57b69f4b20f16ed34e3
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-02 16:04:56 -0800

    Remove personal customization from attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,6 @@ default['boulder']['config']['boulder-config']['va']['portConfig']['httpsPort'] 
 default['boulder']['config']['boulder-config']['va']['portConfig']['tlsPort'] = 443
 default['boulder']['config']['boulder-config']['syslog']['network'] = 'udp'
 default['boulder']['config']['boulder-config']['syslog']['server'] = 'localhost:514'
-# Default 8000 port is needed by Atlassian apps.
-default['boulder']['config']['boulder-config']['wfe']['debugAddr'] = 'localhost:7999'
 
 default['boulder']['config']['issuer-ocsp-responder']['syslog']['network'] = 'udp'
 default['boulder']['config']['issuer-ocsp-responder']['syslog']['server'] = 'localhost:514'
