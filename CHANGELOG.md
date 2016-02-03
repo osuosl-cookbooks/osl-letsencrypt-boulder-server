370a31571bc78a72af61d0242763508936ba7201
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-02 17:26:18 -0800

    Added metadata for Supermarket.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1
+
+* Added metadata for Supermarket.
+
 # 0.1.0
 
 * Initial release of `letsencrypt-boulder-server`, based off
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,12 @@ maintainer_email 'thoutenbos@schubergphilis.com'
 license          'All rights reserved'
 description      "Installs/Configures Boulder, the ACME-based CA server by Let's Encrypt."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+issues_url       'https://github.com/patcon/chef-letsencrypt-boulder-server/issues'
+source_url       'https://github.com/patcon/chef-letsencrypt-boulder-server'
+version          '0.1.1'
+
+supports         'ubuntu'
+supports         'centos'
 
 depends          'golang'
 depends          'rabbitmq'
