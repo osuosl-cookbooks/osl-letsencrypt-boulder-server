65755ff36e365d2cf1716ca43645e28ae7f73545
Lance Albertson <lance@osuosl.org>
2017-06-23 12:26:43 -0700

    Use boulder for hostname during ServerSpec testing

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -2,6 +2,6 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe command('curl http://127.0.0.1:4000/directory') do
+describe command('curl http://boulder:4000/directory') do
   its(:stdout) { should match(%r{"new-cert": "http://127.0.0.1:4000/acme/new-cert"}) }
 end
