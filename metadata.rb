53d64c32a93fa5dc7d7dbf86f69c674abd3cfacf
Jason Chen <jasonc@osuosl.org>
2018-07-14 21:41:03 -0700

    Foodcritic fixes

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'osl-letsencrypt-boulder-server'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
-license          'apachev2'
+license          'Apache-2.0'
+chef_version     '>= 12.18' if respond_to?(:chef_version)
 description      "Installs/Configures Boulder, the ACME-based CA server by Let's Encrypt."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/osuosl-cookbooks/osl-letsencrypt-boulder-server/issues'
