d3e53af4415b4aa88ae2eac6d44c9055a184c1c5
Catherine Stevens <catherine@crimson.workstation.cass.oregonstate.edu>
2019-04-30 11:39:28 -0700

    Migrated from ServerSpec to InSpec

---
+++ test/integration/default/inspec/default_spec.rb
@@ -0,0 +1,8 @@
+describe command('curl http://localhost:4000/directory') do
+  its('stdout') { should match(%r{"new-cert": "http://(localhost|127.0.0.1):4000/acme/new-cert"}) }
+end
+
+describe command('curl https://foo.org -k -v') do
+  its('stderr') { should match(/issuer: CN=h[2a]ppy h[2a]cker fake CA/) }
+  its('stderr') { should match(/common name: foo.org/) }
+end
