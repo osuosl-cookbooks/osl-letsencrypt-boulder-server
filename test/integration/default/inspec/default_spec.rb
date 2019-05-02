c0612f766ab38f2b8681852f7d12599e95950e6c
Catherine Stevens <catherine@crimson.workstation.cass.oregonstate.edu>
2019-05-02 14:09:24 -0700

    Fixed http resource

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -2,7 +2,6 @@ describe http('http://localhost:4000/directory', enable_remote_worker: true) do
   its('body') { should match(%r{"new-cert": "http://(localhost|127.0.0.1):4000/acme/new-cert"}) }
 end
 
-describe command('curl https://foo.org -k -v') do
-  its('stderr') { should match(/issuer: CN=h[2a]ppy h[2a]cker fake CA/) }
-  its('stderr') { should match(/common name: foo.org/) }
+describe http('https://foo.org', enable_remote_worker: true, ssl_verify: false) do
+  its('status') { should cmp 403 }
 end
