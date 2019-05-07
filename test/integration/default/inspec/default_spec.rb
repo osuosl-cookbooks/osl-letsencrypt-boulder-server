dcd409e5434d033a47e47f2db624eb6d48aed962
Catherine Stevns <stevenscatherine2@gmail.com>
2019-05-07 14:23:03 -0700

    Reverted http/command test

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -2,6 +2,16 @@ describe http('http://localhost:4000/directory', enable_remote_worker: true) do
   its('body') { should match(%r{"new-cert": "http://(localhost|127.0.0.1):4000/acme/new-cert"}) }
 end
 
-describe http('https://foo.org', enable_remote_worker: true, ssl_verify: false) do
-  its('status') { should cmp 403 }
+describe command('curl https://foo.org -k -v') do
+  its('stderr') { should match(/issuer: CN=h[2a]ppy h[2a]cker fake CA/) }
+  its('stderr') { should match(/common name: foo.org/) }
 end
+
+#describe http(
+#  'https://localhost',
+#  headers: { 'Hose' => 'foo.org' },
+#  enable_remote_worker: true, 
+#  ssl_verify: false
+#) do
+#  its('status') { should cmp 403 }
+#end
