bdfbece2a2bf793af425b9d6cc29855bab0c8099
David Gasper <dmgasper@osuosl.org>
2019-08-20 16:46:11 -0700

    Chef 14 compatibility fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,14 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ChefSpec::Coverage.start! { add_filter 'osl-letsencrypt-boulder-server' }
-
 CENTOS_6 = {
   platform: 'centos',
-  version: '6.9',
+  version: '6',
 }.freeze
 
 CENTOS_7 = {
   platform: 'centos',
-  version: '7.4.1708',
+  version: '7',
 }.freeze
 
 ALL_PLATFORMS = [
@@ -18,5 +16,5 @@ ALL_PLATFORMS = [
 ].freeze
 
 RSpec.configure do |config|
-  config.log_level = :fatal
+  config.log_level = :warn
 end
