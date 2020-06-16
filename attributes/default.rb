fe6cb2669098986d9bcc0fd68aa19f070f34e3a7
Robert Detjens <detjensrobert@osuosl.org>
2020-06-16 00:17:25 +0000

    cookstyle fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@
 # Cookbook:: chef-letsencrypt-boulder-server
 # Attribute:: default
 #
-# Copyright 2015 Schuberg Philis
+# Copyright:: 2015-2020, Schuberg Philis
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- metadata.rb
+++ metadata.rb
@@ -2,16 +2,13 @@ name             'osl-letsencrypt-boulder-server'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
 license          'Apache-2.0'
-chef_version     '>= 12.18' if respond_to?(:chef_version)
+chef_version     '>= 12.18'
 description      "Installs/Configures Boulder, the ACME-based CA server by Let's Encrypt."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/osuosl-cookbooks/osl-letsencrypt-boulder-server/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-letsencrypt-boulder-server'
 version          '3.1.1'
 
 supports         'centos', '~> 7.0'
-
-depends          'build-essential'
 depends          'git'
 depends          'osl-docker'
 depends          'resolver'
